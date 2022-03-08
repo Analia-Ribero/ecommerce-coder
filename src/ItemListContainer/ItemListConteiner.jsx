f256bc8f5307bf5206f29a3fbcd8e6b202add276
Analia <analiaribero@hotmail.com>
2022-03-07 22:15:25 -0300

    firebaseI

--- src/ItemListContainer/ItemListConteiner.jsx
+++ src/ItemListContainer/ItemListConteiner.jsx
@@ -1,67 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import ItemList from './ItemList'
-
 import { useParams } from 'react-router-dom';
-
-const baseDeDatos =
-  [{
-    "id": 1,
-    "marca": "Samsung",
-    "modelo": "A22",
-    "precio": 32000,
-    "descripcion":"Pantalla. 720 x 1080 pixeles, camara frontal 13 megapixeles, memoria 4GB"
-  }, {
-    "id": 2,
-    "marca": "Motorola",
-    "modelo": "A52",
-    "precio": 63000,
-    "descripcion":"Pantalla. 1080 x 2400 pixeles, camara frontal 32 megapixeles, memoria 6GB"
-  }, {
-    "id": 3,
-    "marca": "Samsung",
-    "modelo": "A72",
-    "precio": 82000,
-    "descripcion":"Pantalla 6.7 pulgadas, camara frontal 32 megapixeles, memoria 6GB"
-  }, {
-    "id": 4,
-    "marca": "Motorola",
-    "modelo": "A02",
-    "precio": 23000,
-    "descripcion":"Pantalla 6.5 pulgadas, camara frontal 5 megapixeles, memoria 2GB"
-  }, {
-    "id": 5,
-    "marca": "LG",
-    "modelo": "A12",
-    "precio": 33000,
-    "descripcion":"Pantalla. 720 x 1080 pixeles, camara frontal 8 megapixeles, memoria 3GB"
-  }]
-  let error = false;
-
-function obtenerBaseDatos (categoriaId){
- 
-  return new Promise ((resolve, reject)=>{
-    setTimeout (
-      ()=> {
-        if(categoriaId){
-        let resultado = baseDeDatos.filter((item)=>{
-          return item.marca === categoriaId;
-        })
-        resolve(resultado);
-      }
-        else resolve (baseDeDatos);
-        },
-      3000);
-      if (error) {
-        reject ( new error ('error en la promise'))
-      } 
-    }
-  )
-}
+import { getAllItems } from '../service/firebase';
 
 
 
 
-export default function ItemListConteiner(props) {
+
+
+export default function ItemListConteiner({props}) {
 
 let {categoriaId} = useParams();
 
@@ -70,18 +17,15 @@ const [item, setItem] = useState ([]);
 
 useEffect (()=>{
   
-  let guardarDatos = obtenerBaseDatos (categoriaId);
-  
-  guardarDatos.then ((itemPromise) => {
-    setItem (itemPromise);
-  }).catch ( (error)=> {
-    console.error(error);
-  }).finally (()=>{
-    console.log("promesa terminada");
-  });
-},[categoriaId])
-
-  return ( 
+  let guardarDatos = categoriaId? getAllItems() : getAllItemsfrom(categoriaId);
+ guardarDatos
+ .then((itemsPromise)=>{
+   setItem(itemsPromise);
+ })
+},
+[categoriaId]
+);
+ return( 
     <>
     <h2>{props.greeting}</h2>
     <section>
---
+++ src/service/firebase.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, colection, getDocs, getDoc, doc } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQVs6ShKf1VHObm2awVTneSrNE5AcuCF4",
+  authDomain: "ecommerce-react-coder.firebaseapp.com",
+  projectId: "ecommerce-react-coder",
+  storageBucket: "ecommerce-react-coder.appspot.com",
+  messagingSenderId: "530662312247",
+  appId: "1:530662312247:web:27bdf6aa1e677bc801f2b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore (app)
+
+export default db
+
+export async function getAllItems(){
+  try {
+    const data = colection (db, "items");
+    const itemsColection = await getDocs(data);
+    const result = itemsColection.docs.map(items=>{
+      return{...items.data(), id : items.id}
+    });
+    return result
+  }
+  catch (err){
+    console.log (err)
+  }
+}
+export async function getItem(itemId){
+try{
+  const data = colection (db,"items");
+  const itemsRef = doc (data, itemId);
+
+  let resultDoc = await getDoc (itemsRef);
+  return{...resultDoc.data(), id : resultDoc.id}
+}
+catch(err){
+  console.log(err)
+}
+}
